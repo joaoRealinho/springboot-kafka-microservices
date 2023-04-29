43fce8fd7c267b43f70c5a4bedc8fe4bf35f50d1
joaoRealinho <joao_ga_io@hotmail.com>
2023-04-30 00:49:10 +0100

    First commit with cve-2022-1471 resolved

---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.example.orderservice.controller;
+
+import com.example.basedomains.dto.Order;
+import com.example.basedomains.dto.OrderEvent;
+import com.example.orderservice.kafka.OrderProducer;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private final OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String createOrder(@RequestBody Order order)
+    {
+        order.setOrderid(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("Order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order placed successfully";
+
+    }
+}
